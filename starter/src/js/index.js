b52f8a1b6a09b6af2dbb3644eab64fdc846bb4e5
Steph <stephaniergreene94@gmail.com>
2019-06-02 21:52:02 +1000

    Set-up dependents + first API call

--- starter/src/js/index.js
+++ starter/src/js/index.js
@@ -1,1 +1,16 @@
 // Global app controller
+import axios from 'axios';
+async function getResults(query) {
+    const proxy = 'https://cors-anywhere.herokuapp.com/';
+    const key = '19d5e710250668f8d2e808113ca60d4d';
+    try {
+        const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${query}`)
+        const recipes = res.data.recipes
+        console.log(recipes);
+    } catch (error) {
+    alert(error);
+    }
+}
+getResults('pizza');
+
+
