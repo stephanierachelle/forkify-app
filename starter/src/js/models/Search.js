618194b5f4afdbe8f95b15ed7ef738ef424f4554
Steph <stephaniergreene94@gmail.com>
2019-06-03 22:23:01 +1000

    Set-up main controllers

--- starter/src/js/models/Search.js
+++ starter/src/js/models/Search.js
@@ -1,1 +1,22 @@
-export default 'I am an exported string.';
+import axios from 'axios';
+
+export default class Search {
+    constructor(query) {
+        this.query = query;
+    }
+
+    async getResults() {
+        const proxy = 'https://cors-anywhere.herokuapp.com/';
+        const key = '19d5e710250668f8d2e808113ca60d4d';
+        try {
+            const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${this.query}`)
+            console.log(res)
+            this.result = res.data.recipes;
+            // console.log(this.result);
+        } catch (error) {
+        alert(error);
+        }
+    }
+}
+
+
